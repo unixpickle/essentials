e179ab7ba9733c120c7f9ed3e1ebbc298d2a93ad
Alex Nichol <unixpickle@gmail.com>
2017-02-12 11:57:44 -0500

    AddCtxTo method

--- errors.go
+++ errors.go
@@ -16,6 +16,21 @@ func AddCtx(ctx string, err error) error {
 	return &CtxError{Context: ctx, Original: err}
 }
 
+// AddCtxTo is like doing
+//
+//     *err = AddCtx(ctx, *err)
+//
+// It is useful for adding context to a named return
+// argument, like in:
+//
+//     func MyMethod() (err error) {
+//         defer essentials.AddCtxTo("MyMethod", &err)
+//         // Code here...
+//     }
+func AddCtxTo(ctx string, err *error) {
+	*err = AddCtx(ctx, *err)
+}
+
 // Error returns an error message with added context.
 // The message is of the form "Context: Original.Error()".
 func (c *CtxError) Error() string {
